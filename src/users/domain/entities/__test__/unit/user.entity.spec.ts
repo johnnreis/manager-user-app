493b0f6b9efa65a236fb4843372352ab91a829c8
John Reis <jhonatareisy7@gmail.com>
2025-03-15 00:33:27 -0300

    userEntity constructor unit test

---
+++ src/users/domain/entities/__test__/unit/user.entity.spec.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+import { UserEntity, UserProps } from '../../user.entity'
+describe('UserEntity unit test', () => {
+  let props: UserProps
+  let sut: UserEntity
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+    sut = new UserEntity(props)
+  })
+  it('should ensure that UserEntity is instantiaded', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+})
