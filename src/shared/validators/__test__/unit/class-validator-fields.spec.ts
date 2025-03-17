3d0c7c3608f22b94dd6b61240b9fa156517ba93e
John Reis <jhonatareisy7@gmail.com>
2025-03-17 12:15:35 -0300

    test: test for invalidation cases password field

--- src/shared/validators/__test__/unit/class-validator-fields.spec.ts
+++ src/shared/validators/__test__/unit/class-validator-fields.spec.ts
@@ -1,64 +1,63 @@
-
-import * as libClassValidator from "class-validator";
-import { ClassValidatorFields } from "../../class-validator-fields";
+import * as libClassValidator from 'class-validator'
+import { ClassValidatorFields } from '../../class-validator-fields'
 
 class StubClassValidatorFields extends ClassValidatorFields<StubRules> {}
 
 class StubRules {
   @libClassValidator.IsString()
   @libClassValidator.IsNotEmpty()
-  field: string;
+  field: string
 
   constructor(field: string) {
-    this.field = field;
+    this.field = field
   }
 }
 
 describe('ClassValidatorFields unit tests', () => {
-  let sut: StubClassValidatorFields;
+  let sut: StubClassValidatorFields
 
   beforeEach(() => {
-    sut = new StubClassValidatorFields();
-  });
+    sut = new StubClassValidatorFields()
+  })
 
   it('Should start with undefined errors and validatedData', () => {
     const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
 
     spyValidateSync.mockReturnValue([
-     { property: 'field', constraints: {isRequired: 'test error'}}
+      { property: 'field', constraints: { isRequired: 'test error' } },
     ])
 
-    expect(sut.errors).toBeUndefined();
-    expect(sut.validatedData).toBeUndefined();
+    expect(sut.errors).toBeUndefined()
+    expect(sut.validatedData).toBeUndefined()
     expect(sut.validate(null)).toBeFalsy()
     expect(spyValidateSync).toHaveBeenCalled()
-    expect(sut.errors).toStrictEqual({field: ['test error']})
-  });
+    expect(sut.errors).toStrictEqual({ field: ['test error'] })
+  })
 
   it('Should validate with errors', () => {
-    const invalidData = new StubRules('');
-    const isValid = sut.validate(invalidData);
+    const invalidData = new StubRules('')
+    const isValid = sut.validate(invalidData)
 
-    expect(isValid).toBeFalsy();
-    expect(sut.errors).toBeDefined();
-    expect(sut.validatedData).toBeUndefined();
-  });
+    expect(isValid).toBeFalsy()
+    expect(sut.errors).toBeDefined()
+    expect(sut.validatedData).toBeUndefined()
+  })
 
   it('Should validate without errors', () => {
     const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
-    spyValidateSync.mockReturnValue([ ])
+    spyValidateSync.mockReturnValue([])
     const sut = new StubClassValidatorFields()
 
-    const validData = new StubRules('valid_value');
-    const isValid = sut.validate(validData);
+    const validData = new StubRules('valid_value')
+    const isValid = sut.validate(validData)
 
-    expect(isValid).toBeTruthy();
-    expect(sut.validatedData).toEqual(validData);
-    expect(sut.errors).toBeUndefined();
-
-    expect(sut.validate({field: 'value'})).toBeTruthy()
-    expect(spyValidateSync).toHaveBeenCalled()
-    expect(sut.validatedData).toStrictEqual({field: 'value'})
+    expect(isValid).toBeTruthy()
+    expect(sut.validatedData).toEqual(validData)
     expect(sut.errors).toBeUndefined()
-  });
-});
+
+    expect(sut.validate({ field: 'value' })).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({ field: 'value' })
+    expect(sut.errors).toBeUndefined()
+  })
+})
--- src/shared/validators/class-validator-fields.ts
+++ src/shared/validators/class-validator-fields.ts
@@ -7,22 +7,21 @@ import {
 export abstract class ClassValidatorFields<PropsValidated>
   implements ValidatorFieldsInterface<PropsValidated>
 {
-  errors?: FieldsErrors
-  validatedData?: PropsValidated
+  errors: FieldsErrors
+  validatedData: PropsValidated
 
   validate(data: any): boolean {
-      const errors = validateSync(data)
+    const errors = validateSync(data)
 
-      if(errors.length) {
-        this.errors = {}
-        for (const error of errors) {
-          const field = error.property
-          this.errors[field] = Object.values(error.constraints || {})
-        }
-      } else {
-        this.validatedData = data
-        this.errors = undefined;
+    if (errors.length) {
+      this.errors = {}
+      for (const error of errors) {
+        const field = error.property
+        this.errors[field] = Object.values(error.constraints || {})
       }
-      return !errors.length
+    } else {
+      this.validatedData = data
+    }
+    return !errors.length
   }
 }
